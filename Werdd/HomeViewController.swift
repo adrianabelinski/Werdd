e1c2f6c1675c42288af98b85e33251a2ddf04b25
Adriana Belinski <adrianabmpls@gmail.com>
2022-10-16 13:35:14 -0500

    Updated code: removed stacks, cleaned code, & fixed setUpContainerView.

--- Werdd/HomeViewController.swift
+++ Werdd/HomeViewController.swift
@@ -8,146 +8,140 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+  // UIViewController class part of uikit framework. Anything that beguns with ui is part of uikit
   
-  let blueView: UIView = {
-    let view = UIView()
-    view.backgroundColor = UIColor(red: 0.208, green: 0.506, blue: 0.722, alpha: 0.6) // #3581b8
-    view.translatesAutoresizingMaskIntoConstraints = false
-    view.layer.cornerRadius = 25
-    return view
-  }()
-  
-  
-  let stackViewVertical: UIStackView = {
-    let stackView = UIStackView()
-    stackView.translatesAutoresizingMaskIntoConstraints = false
-    stackView.axis = .vertical
-    stackView.spacing = 10
-    stackView.alignment = .leading
-    stackView.layer.masksToBounds = true
-    return stackView
-  }()
-  
-  let stackViewHorizontal: UIStackView = {
-    let stackView = UIStackView()
-    stackView.translatesAutoresizingMaskIntoConstraints = false
-    stackView.axis = .horizontal
-    stackView.spacing = 10
-    stackView.alignment = .bottom
-    stackView.layer.masksToBounds = true
-    return stackView
-  }()
-  
-  let padding: CGFloat = 20
-
-
-  let labelOne: UILabel = {
-      let label = UILabel()
-      label.translatesAutoresizingMaskIntoConstraints = false
-      label.text = "Programming"
-      label.font = UIFont(name: "Rubik-Bold", size: 30)
-      label.textAlignment = .center
-      return label
-  }()
-  
-  let labelTwo: UILabel = {
-      let label = UILabel()
-      label.translatesAutoresizingMaskIntoConstraints = false
-      label.text = "noun"
-      label.font = UIFont(name: "Rubik-Regular", size: 8)
-      label.textAlignment = .center
-      return label
-  }()
-  
-  let labelThree: UILabel = {
-      let label = UILabel()
-      label.translatesAutoresizingMaskIntoConstraints = false
-      label.text = "creating a sequence of instructions to enable the computer to do something."
-      label.lineBreakMode = .byWordWrapping
-      label.font = UIFont(name: "Rubik-Regular", size: 10)
-      label.numberOfLines = 3
-      return label
-  }()
-  
-  
-  
-  let randomButton: UIButton = {
-    let largeConfig = UIImage.SymbolConfiguration(
-      pointSize: 140,
-      weight: .regular,
-      scale: .large
-    )
-    
-    let image = UIImage(
-      systemName: "arrow.clockwise.circle",
-      withConfiguration: largeConfig
-    )?.withTintColor(.white, renderingMode: .alwaysTemplate)
-    
-    let button = UIButton()
-    button.translatesAutoresizingMaskIntoConstraints = false
-    button.setImage(image, for: .normal)
-    button.tintColor = .white
-    return button
-  }()
-  
-  
-  
+  // MARK: - Properties
   
   func setUpNavigationTitle() {
     title = "Werdd"
     navigationController?.navigationBar.prefersLargeTitles = true
   }
-
   
+  let containerView: UIView = {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.backgroundColor = UIColor(named: "Navy")
+    view.layer.cornerRadius = 20
+    return view
+  }()
+  
+  let wordTitleLabel: UILabel = {
+      let label = UILabel()
+      label.translatesAutoresizingMaskIntoConstraints = false
+      label.text = "Programming"
+      label.font = UIFont(name: "Rubik-Bold", size: 24)
+      return label
+  }()
+  
+  let partsOfSpeechLabel: UILabel = {
+      let label = UILabel()
+      label.translatesAutoresizingMaskIntoConstraints = false
+      label.text = "noun"
+      label.font = UIFont(name: "Rubik-Italic", size: 14)
+      return label
+  }()
+  
+  let wordDefinitionLabel: UILabel = {
+      let label = UILabel()
+      label.translatesAutoresizingMaskIntoConstraints = false
+      label.text = "creating a sequence of instructions to enable the computer to do something."
+      label.font = UIFont(name: "Rubik-Light", size: 18)
+      label.lineBreakMode = .byWordWrapping
+      label.numberOfLines = 0
+      return label
+  }()
+  
+  
+  let randomButton: UIButton = {
+      let largeConfig = UIImage.SymbolConfiguration(
+      pointSize: 140,
+      weight: .regular,
+      scale: .large
+    )
+    
+      let image = UIImage(
+      systemName: "arrow.clockwise.circle",
+      withConfiguration: largeConfig
+    )?.withTintColor(.white, renderingMode: .alwaysTemplate)
+    
+      let button = UIButton()
+      button.translatesAutoresizingMaskIntoConstraints = false
+      button.setImage(image, for: .normal)
+      button.tintColor = .white
+      return button
+  }()
+  
+  // MARK: - Lifecycle
+  
+  let padding: CGFloat = 20
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = UIColor(red: 0.914, green: 0.902, blue: 0.894, alpha: 1) // #e9e6e4
-    setUpNavigationTitle() //title sets up
-    addSubviews()
     
-  }
+    view.backgroundColor = UIColor(named: "Taupe")
 
-  func addSubviews() {
-   // view.addSubview(titleLabel) //view is an aspect of UIView controller
-    view.addSubview(blueView)
-    blueView.addSubview(randomButton)
-    blueView.addSubview(stackViewHorizontal)
-    blueView.addSubview(labelThree)
-    
-    NSLayoutConstraint.activate([
-      blueView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
-      blueView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-      blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-      blueView.heightAnchor.constraint(equalTo: blueView.widthAnchor),
-      
-      stackViewHorizontal.topAnchor.constraint(equalTo: blueView.topAnchor, constant: padding),
-      stackViewHorizontal.leadingAnchor.constraint(equalTo: blueView.leadingAnchor, constant: padding),
-      
-     // labelTwo.topAnchor.constraint(equalTo: labelOne.centerYAnchor, constant: 0),
-      
-      labelThree.topAnchor.constraint(equalTo: stackViewHorizontal.bottomAnchor, constant: padding),
-      labelThree.leadingAnchor.constraint(equalTo: blueView.leadingAnchor, constant: padding),
-      labelThree.trailingAnchor.constraint(equalTo: blueView.trailingAnchor, constant: -padding),
-      
-      randomButton.bottomAnchor.constraint(equalTo: blueView.bottomAnchor, constant: -padding),
-      randomButton.trailingAnchor.constraint(equalTo: blueView.trailingAnchor, constant: -padding),
-      randomButton.heightAnchor.constraint(equalToConstant: 50),
-      randomButton.widthAnchor.constraint(equalToConstant: 50),
-      
-      
-                ])
-      
-    stackViewHorizontal.addArrangedSubview(labelOne)
-    stackViewHorizontal.addArrangedSubview(labelTwo)
-    
-  //  stackView.addArrangedSubview(labelThree)
-  //  stackView.addArrangedSubview(randomButton)
-
-    
+    setUpNavigationTitle()
+    setUpUI()
   }
   
-}
+  // MARK: - UI Setup
 
-//make button own floating thing and pin to bottom right of blue view.
-//May need to  not use stacks on label one and two.
+  func setUpUI() { //view is an aspect of UIView controller
+    setUpContainerView()
+    setUpWordTitle()
+    setUpPartsOfSpeech()
+    setUpDefinition()
+    setUpRandomButton()
+  }
+
+  func setUpContainerView() {
+      view.addSubview(containerView)
+      
+      NSLayoutConstraint.activate([
+          containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+          containerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+          containerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+          containerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3)
+      ])
+  }
+  
+  func setUpWordTitle() {
+      containerView.addSubview(wordTitleLabel)
+      
+      NSLayoutConstraint.activate([
+          wordTitleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
+          wordTitleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20)
+      ])
+  }
+  
+  func setUpPartsOfSpeech() {
+      containerView.addSubview(partsOfSpeechLabel)
+      
+      NSLayoutConstraint.activate([
+          partsOfSpeechLabel.bottomAnchor.constraint(equalTo: wordTitleLabel.bottomAnchor, constant: -4),
+          partsOfSpeechLabel.leadingAnchor.constraint(equalTo: wordTitleLabel.trailingAnchor, constant: 5),
+          partsOfSpeechLabel.trailingAnchor.constraint(lessThanOrEqualTo: containerView.trailingAnchor)
+      ])
+  }
+  
+  func setUpDefinition() {
+      containerView.addSubview(wordDefinitionLabel)
+      
+      NSLayoutConstraint.activate([
+          wordDefinitionLabel.topAnchor.constraint(equalTo: partsOfSpeechLabel.bottomAnchor, constant: 20),
+          wordDefinitionLabel.leadingAnchor.constraint(equalTo: wordTitleLabel.leadingAnchor),
+          wordDefinitionLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20)
+      ])
+  }
+  
+    func setUpRandomButton() {
+      containerView.addSubview(randomButton)
+      
+      NSLayoutConstraint.activate([
+        randomButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -padding),
+        randomButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+        randomButton.heightAnchor.constraint(equalToConstant: 50),
+        randomButton.widthAnchor.constraint(equalToConstant: 50),
+        ])
+  }
+}
