8e2737bd12c748b67faa5bfafdf01afc42ff8d50
Adriana Belinski <adrianabmpls@gmail.com>
2022-10-19 10:52:29 -0500

    Fixed indentation.

--- Werdd/HomeViewController.swift
+++ Werdd/HomeViewController.swift
@@ -27,45 +27,45 @@ class HomeViewController: UIViewController {
   }()
   
   let wordTitleLabel: UILabel = {
-      let label = UILabel()
-      label.translatesAutoresizingMaskIntoConstraints = false
-      label.font = UIFont(name: "Rubik-Bold", size: 24)
-      return label
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.font = UIFont(name: "Rubik-Bold", size: 24)
+    return label
   }()
   
   let partsOfSpeechLabel: UILabel = {
-      let label = UILabel()
-      label.translatesAutoresizingMaskIntoConstraints = false
-      label.font = UIFont(name: "Rubik-Italic", size: 14)
-      return label
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.font = UIFont(name: "Rubik-Italic", size: 14)
+    return label
   }()
   
   let wordDefinitionLabel: UILabel = {
-      let label = UILabel()
-      label.translatesAutoresizingMaskIntoConstraints = false
-      label.font = UIFont(name: "Rubik-Light", size: 18)
-      label.lineBreakMode = .byWordWrapping
-      label.numberOfLines = 0
-      return label
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.font = UIFont(name: "Rubik-Light", size: 18)
+    label.lineBreakMode = .byWordWrapping
+    label.numberOfLines = 0
+    return label
   }()
   
   let randomButton: UIButton = {
-      let largeConfig = UIImage.SymbolConfiguration(
+    let largeConfig = UIImage.SymbolConfiguration(
       pointSize: 140,
       weight: .regular,
       scale: .large
     )
     
-      let image = UIImage(
+    let image = UIImage(
       systemName: "arrow.clockwise.circle",
       withConfiguration: largeConfig
     )?.withTintColor(.white, renderingMode: .alwaysTemplate)
     
-      let button = UIButton()
-      button.translatesAutoresizingMaskIntoConstraints = false
-      button.setImage(image, for: .normal)
-      button.tintColor = .white
-      return button
+    let button = UIButton()
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.setImage(image, for: .normal)
+    button.tintColor = .white
+    return button
   }()
   
   let dictionaryTableView: UITableView = {
@@ -76,7 +76,7 @@ class HomeViewController: UIViewController {
   
   
   let padding: CGFloat = 20
-
+  
   
   // MARK: - Lifecycle
   
@@ -87,20 +87,20 @@ class HomeViewController: UIViewController {
     
     dictionaryTableView.delegate = self
     
-   // view.addSubview(allDictionaryWords)
+    // view.addSubview(allDictionaryWords)
     
     view.backgroundColor = UIColor(named: "Taupe")
-
+    
     setUpNavigationTitle()
     setUpUI()
     
     if let word = Dictionary.allWords.first {
-    updateViews(withWord: word)
+      updateViews(withWord: word)
     }
   }
   
   // MARK: - UI Setup
-
+  
   func setUpUI() { //view is an aspect of UIView controller
     setUpContainerView()
     setUpWordTitle()
@@ -109,63 +109,63 @@ class HomeViewController: UIViewController {
     setUpRandomButton()
     setUpDictionaryTableView()
   }
-
+  
   func setUpContainerView() {
-      view.addSubview(containerView)
-      
-      NSLayoutConstraint.activate([
-          containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-          containerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-          containerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-          containerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3)
-      ])
+    view.addSubview(containerView)
+    
+    NSLayoutConstraint.activate([
+      containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+      containerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+      containerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+      containerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3)
+    ])
   }
   
   func setUpWordTitle() {
-      containerView.addSubview(wordTitleLabel)
-      
-      NSLayoutConstraint.activate([
-          wordTitleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
-          wordTitleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20)
-      ])
+    containerView.addSubview(wordTitleLabel)
+    
+    NSLayoutConstraint.activate([
+      wordTitleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
+      wordTitleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20)
+    ])
   }
   
   func setUpPartsOfSpeech() {
-      containerView.addSubview(partsOfSpeechLabel)
-      
-      NSLayoutConstraint.activate([
-          partsOfSpeechLabel.bottomAnchor.constraint(equalTo: wordTitleLabel.bottomAnchor, constant: -4),
-          partsOfSpeechLabel.leadingAnchor.constraint(equalTo: wordTitleLabel.trailingAnchor, constant: 5),
-          partsOfSpeechLabel.trailingAnchor.constraint(lessThanOrEqualTo: containerView.trailingAnchor)
-      ])
+    containerView.addSubview(partsOfSpeechLabel)
+    
+    NSLayoutConstraint.activate([
+      partsOfSpeechLabel.bottomAnchor.constraint(equalTo: wordTitleLabel.bottomAnchor, constant: -4),
+      partsOfSpeechLabel.leadingAnchor.constraint(equalTo: wordTitleLabel.trailingAnchor, constant: 5),
+      partsOfSpeechLabel.trailingAnchor.constraint(lessThanOrEqualTo: containerView.trailingAnchor)
+    ])
   }
   
   func setUpDefinition() {
-      containerView.addSubview(wordDefinitionLabel)
-      
-      NSLayoutConstraint.activate([
-          wordDefinitionLabel.topAnchor.constraint(equalTo: partsOfSpeechLabel.bottomAnchor, constant: 20),
-          wordDefinitionLabel.leadingAnchor.constraint(equalTo: wordTitleLabel.leadingAnchor),
-          wordDefinitionLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20)
-      ])
+    containerView.addSubview(wordDefinitionLabel)
+    
+    NSLayoutConstraint.activate([
+      wordDefinitionLabel.topAnchor.constraint(equalTo: partsOfSpeechLabel.bottomAnchor, constant: 20),
+      wordDefinitionLabel.leadingAnchor.constraint(equalTo: wordTitleLabel.leadingAnchor),
+      wordDefinitionLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20)
+    ])
   }
   
-    func setUpRandomButton() {
-      containerView.addSubview(randomButton)
+  func setUpRandomButton() {
+    containerView.addSubview(randomButton)
     
-      randomButton.addTarget(self, action: #selector(randomButtonPressed), for: .touchUpInside)
-      
-      NSLayoutConstraint.activate([
-        randomButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -padding),
-        randomButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
-        randomButton.heightAnchor.constraint(equalToConstant: 50),
-        randomButton.widthAnchor.constraint(equalToConstant: 50),
-        ])
+    randomButton.addTarget(self, action: #selector(randomButtonPressed), for: .touchUpInside)
+    
+    NSLayoutConstraint.activate([
+      randomButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -padding),
+      randomButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+      randomButton.heightAnchor.constraint(equalToConstant: 50),
+      randomButton.widthAnchor.constraint(equalToConstant: 50),
+    ])
   }
   
   func setUpDictionaryTableView() {
     containerView.addSubview(dictionaryTableView)
-
+    
     NSLayoutConstraint.activate([
       dictionaryTableView.topAnchor.constraint(equalTo: view.topAnchor),
       dictionaryTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -187,9 +187,9 @@ class HomeViewController: UIViewController {
   }
   
   func updateViews(withWord word: Entry) {
-      wordTitleLabel.text = word.wordTitle
-      partsOfSpeechLabel.text = word.partsOfSpeech
-      wordDefinitionLabel.text = word.wordDefinition
+    wordTitleLabel.text = word.wordTitle
+    partsOfSpeechLabel.text = word.partsOfSpeech
+    wordDefinitionLabel.text = word.wordDefinition
   }
 }
 
@@ -198,7 +198,7 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-   // return 10
+    // return 10
     return Dictionary.allWords.count
   }
   
@@ -208,7 +208,7 @@ extension HomeViewController: UITableViewDataSource {
     
     content.text = Dictionary.allWords[indexPath.row].wordTitle
     content.secondaryText = Dictionary.allWords[indexPath.row].wordDefinition
-
+    
     cell.contentConfiguration = content
     return cell
   }
