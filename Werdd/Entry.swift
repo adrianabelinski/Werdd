a73a922aa7d1ad67be53e33f834195e67759aa70
Adriana Belinski <adrianabmpls@gmail.com>
2022-11-05 19:09:30 -0500

    Added Entry file.

---
+++ Werdd/Entry.swift
@@ -0,0 +1,14 @@
+//
+//  Entry.swift
+//  Werdd
+//
+//  Created by Adriana Belinski on 11/5/22.
+//
+
+import Foundation
+
+struct Entry {
+  let wordTitle: String
+  let partsOfSpeech: String
+  let wordDefinition: String
+}
